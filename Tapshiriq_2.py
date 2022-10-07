94c41479dfb7fcab045008c62dca7f80cfeaa6c8
JamilKhalilov <114008754+JamilKhalilov@users.noreply.github.com>
2022-10-07 21:21:38 +0400

    Add files via upload

---
+++ Tapshiriq_2.py
@@ -0,0 +1,11 @@
+class ishchiler:
+    def person(self,ad,soyad,yash):
+        
+class programistler:
+    def person(self,ad,soyad,yash):
+        
+class directorlar:
+    def person(self,ad,soyad,yash):       
+        
+class investorlar:
+    def person(self,ad,soyad,yash):
